a741dcb15087ccb1195a01f00256968b6351c2a2
ManuelJuliao <manuelcjuliao@gmail.com>
2023-12-04 16:12:49 +0000

    added extensions and corrected new folder path

--- download_org.py
+++ download_org.py
@@ -5,7 +5,9 @@ from pathlib import Path
 subdir = {
         "Docs":[".pdf",".docx",".txt"],
         "Audio":[".m4a",".m4b",".mp3"],
-        "Images":[".jpg",".jpeg",".png"]
+        "Images":[".jpg",".jpeg",".png",".jfif",".avif"],
+        "Apps":[".exe",".msi"],
+        "Compressed":[".zip",".rar"]
         }
 
 # function to pick folder given file extension
@@ -16,7 +18,7 @@ def pickDir(ext):
                 return folder
 
 
-file_path = 'C:\Downloads'
+file_path = "C:/Users/manuel/Downloads"
 
 # function that scans all files, check extension and move it to respective folder
 def organizeDir():
@@ -28,19 +30,25 @@ def organizeDir():
         
         # save file path
         filePath = Path(item)
+        print(filePath)
         # save file extension
         fileType = filePath.suffix.lower()
+        print(fileType)
         directory = pickDir(fileType)
+        print(directory)
         
         # skip if file doesn't have extension
         if directory == None:
             continue
         
-        directoryPath = Path(directory)
+        directoryPath = Path(file_path)/directory
+        print(directoryPath)
         # make new directory if not found
         if directoryPath.is_dir() != True:
                 directoryPath.mkdir()
+                print(directoryPath)
         # move file 
-        filePath.rename(directoryPath.joinpath(filePath))
+        newFilePath = directoryPath / filePath.name
+        filePath.rename(newFilePath)
 
 organizeDir()
